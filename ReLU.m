297a849ebfab95cc37f5ab9398c2b871dec5655b
bcc5160 <bc.chiu12@gmail.com>
2016-09-12 14:31:53 -0400

    gettings tim's image norm

---
+++ ReLU.m
@@ -0,0 +1,33 @@
+% Course:   CMPEN 454   Section 1
+% Authors:  Collin Baker
+%           Brittany Chiu
+%           Tim Lagnese
+%           Anthony Marucci
+% Date:     September 21, 2016
+%
+% Project 1
+%
+% File:     ReLU.m
+
+% Note:     ReLU stands for Rectified Linear Unit, but despite the fancy name it is just a thresholding operation
+%               where any negative numbers in the input become 0 in the output.
+% Input:    A color image array in  of size N x M x D where N, M > 0.
+% Output:   A modified matrix in which all the values less than 0 are
+%               changed to 0 (result: totally nonegative matrix)
+function [ out ] = ReLU(in)
+
+    out = in;
+    
+    % Get the last dimension of the image matrix 
+    %   ndims(D) = Number of dimensions in image (which should be 3)
+    %   ndims(ndims(D)) = Get the number of D in channels from total number 
+    %   of dimensions, which again should be 3 (how many in last 1,1,:),
+    %   
+    D = ndims(ndims(in));
+
+    for k=1:D;
+        out = max(in, 0);
+    end
+
+end
+
---
+++ SoftMax.m
@@ -0,0 +1,37 @@
+% Course:   CMPEN 454   Section 1
+% Authors:  Collin Baker
+%           Brittany Chiu
+%           Tim Lagnese
+%           Anthony Marucci
+% Date:     September 21, 2016
+%
+% Project 1
+%
+% File:     SoftMax.m
+
+% Input:    A color image array in  of size 1 x 1 x D where D > 0.
+% Output:   An array of dimensions 1,1,k but each value is a probability
+%            and they all addd to 1.
+function [ out ] = SoftMax( in )
+%   (Note: For each "channel", k/Sum of all k. Return all 3D of matrix)
+    
+    % Get last dimension
+    D = ndims(ndims(in));
+    
+    % alpha is the max of the first row, first column of all three
+    % channels, 1 to k
+    alpha = max(in(1,1,:));
+    
+    % out should match in, we are now going to change the values
+    out = in;
+    sum = 0;
+    
+    for k=1:D;
+        sum = exp(in(1,1,k)-alpha);
+    end
+    
+    for k=1:D
+       out(1,1,k) = ( exp(in(1,1,k)-alpha) )/ sum;
+    end
+end
+
