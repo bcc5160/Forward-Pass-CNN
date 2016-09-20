8fd7d147b0c3b8d320a9eaba7282064f9b2a454e
Collin Baker <chbaker0@gmail.com>
2016-09-19 22:44:16 -0400

    Add code to test all layers

--- TestLayers.m
+++ TestLayers.m
@@ -5,7 +5,20 @@ function out = TestLayers(imrgb, filterbanks, biasvectors, layerResults)
     @(x)Convolution(x, filterbanks{2}, biasvectors{2}),
     @(x)ReLU(x),
     @(x)Convolution(x, filterbanks{4}, biasvectors{4}),
-    @(x)ReLU(x)
+    @(x)ReLU(x),
+    @(x)Maxpool(x),
+    @(x)Convolution(x, filterbanks{7}, biasvectors{7}),
+    @(x)ReLU(x),
+    @(x)Convolution(x, filterbanks{9}, biasvectors{9}),
+    @(x)ReLU(x),
+    @(x)Maxpool(x),
+    @(x)Convolution(x, filterbanks{12}, biasvectors{12}),
+    @(x)ReLU(x),
+    @(x)Convolution(x, filterbanks{14}, biasvectors{14}),
+    @(x)ReLU(x),
+    @(x)Maxpool(x),
+    @(x)FullyConnected(x, filterbanks{17}, biasvectors{17}),
+    @(x)SoftMax(x)
   };
   
   prev = imrgb;
