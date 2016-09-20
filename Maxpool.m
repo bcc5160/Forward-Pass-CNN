7eaa432cba1ef24ab55a89e2e588c8362e05a821
Collin Baker <chbaker0@gmail.com>
2016-09-19 22:43:27 -0400

    Fix bugs in Maxpool

--- Maxpool.m
+++ Maxpool.m
@@ -1,11 +1,16 @@
 function [output] = Maxpool (input)
 
-[n, m, d] = size (input);
+outSize = size (input);
+outSize(1) = outSize(1) / 2;
+outSize(2) = outSize(2) / 2;
 
-for i = 1:n
-  for j = 1:m
-    for d = 1:d
-      output(i,j,d) = max(input(2*i-1:2*i,2*j+1:2*j,1));
+output = zeros(outSize);
+
+for i = 1:outSize(1)
+  for j = 1:outSize(2)
+    for d = 1:outSize(3)
+      temp = input(2*i-1:2*i,2*j-1:2*j,d);
+      output(i,j,d) = max(temp(:));
     endfor
   endfor
 endfor
